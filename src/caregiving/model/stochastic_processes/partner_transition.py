44d2d1a111b49688be1012208e0d22efeb00486c
segsell <gsell.sebastian@gmail.com>
2025-01-24 17:15:44 +0100

    Add specs folder

--- src/caregiving/model/stochastic_processes/partner_transition.py
+++ src/caregiving/model/stochastic_processes/partner_transition.py
@@ -1,8 +1,9 @@
 """Partner transition process."""
 
 
-def partner_transition(period, education, has_partner, options):
+def partner_transition(period, education, partner_state, options):
+    """Transition probability for next period partner state."""
     trans_mat = options["partner_trans_mat"]
-    trans_vector = trans_mat[education, period, has_partner]
+    trans_vector = trans_mat[education, period, partner_state]
 
     return trans_vector
