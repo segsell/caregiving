cb1365f0f300c4823d4eb6bb7ed39fb6df5a2fb4
segsell <gsell.sebastian@gmail.com>
2025-08-13 18:47:33 +0200

    Additive (dis)utility of children and work

--- src/caregiving/model/utility/utility_functions.py
+++ src/caregiving/model/utility/utility_functions.py
@@ -164,7 +164,7 @@ def utility_func_alive(
         jnp.log(consumption * eta / cons_scale),
         utility_rho_not_one,
     )
-    return utility * disutil_children  # + zeta * care_demand
+    return utility + disutil_children  # + zeta * care_demand
 
 
 # def _utility_func_alive(
@@ -342,15 +342,15 @@ def marg_utility(
         params=params,
         options=options,
     )
-    disutil_children = disutility_of_children_and_work(
-        period=period,
-        choice=choice,
-        education=education,
-        partner_state=partner_state,
-        health=health,
-        params=params,
-        options=options,
-    )
+    # disutil_children = disutility_of_children_and_work(
+    #     period=period,
+    #     choice=choice,
+    #     education=education,
+    #     partner_state=partner_state,
+    #     health=health,
+    #     params=params,
+    #     options=options,
+    # )
     # disutil_children = 1
 
     marg_util_rho_not_one = ((eta / cons_scale) ** (1 - rho)) * (consumption ** (-rho))
@@ -361,7 +361,7 @@ def marg_utility(
         marg_util_rho_not_one,
     )
 
-    return marg_util * disutil_children
+    return marg_util  # * disutil_children
 
 
 def marg_utility_adda(
@@ -425,16 +425,16 @@ def inverse_marginal(
         params=params,
         options=options,
     )
-    disutil_children = disutility_of_children_and_work(
-        period=period,
-        choice=choice,
-        education=education,
-        partner_state=partner_state,
-        health=health,
-        params=params,
-        options=options,
-    )
-    # disutil_children = 1
+    # disutil_children = disutility_of_children_and_work(
+    #     period=period,
+    #     choice=choice,
+    #     education=education,
+    #     partner_state=partner_state,
+    #     health=health,
+    #     params=params,
+    #     options=options,
+    # )
+    disutil_children = 1
 
     base = marginal_utility ** (-1.0 / rho) * (eta / cons_scale) ** ((1.0 - rho) / rho)
     # consumption_rho_not_one = marginal_utility ** (-1 / rho) * (eta / cons_scale) ** (
@@ -451,7 +451,7 @@ def inverse_marginal(
         consumption_rho_not_one,
     )
 
-    return consumption * disutil_children
+    return consumption  # * disutil_children
 
 
 def inverse_marginal_adda(
