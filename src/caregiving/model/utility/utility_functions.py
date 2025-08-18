762462cf27842928a6c93ff5261d07956151135d
segsell <gsell.sebastian@gmail.com>
2025-08-18 14:31:17 +0200

    Reciprocal of square root

--- src/caregiving/model/utility/utility_functions.py
+++ src/caregiving/model/utility/utility_functions.py
@@ -872,9 +872,9 @@ def disutility_of_children_and_work(
 
 def _func_age_of_youngest_child(age_youngest_child, params):
     # return age_youngest_child
-    return jnp.sqrt(age_youngest_child)
+    # return jnp.sqrt(age_youngest_child)
     # return jnp.log(age_youngest_child + 1)
-    # return 1 / jnp.sqrt(age_youngest_child)
+    return 1 / jnp.sqrt(age_youngest_child)
     # return jnp.log(params["age_of_youngest_child_curvature"] * age_youngest_child + 1)
 
 
