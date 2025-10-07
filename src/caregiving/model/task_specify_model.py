946427b64700699efbfbe7c2a51d7520bef889f5
segsell <gsell.sebastian@gmail.com>
2025-10-07 08:42:25 +0200

    Back to where we were

--- src/caregiving/model/task_specify_model.py
+++ src/caregiving/model/task_specify_model.py
@@ -102,6 +102,7 @@ def task_specify_model(
                     "transition": care_demand_and_supply_transition,
                     "states": np.arange(3, dtype=int),
                 },
+                #
             },
             "continuous_states": {
                 "wealth": savings_grid,
